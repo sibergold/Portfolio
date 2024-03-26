f341aa9736d55309036a5c3eef2b0f056039a919
sibergold <kaynakca21@hotmail.com>
2024-03-27 02:30:52 +0300

    second commit

--- portfolio.js
+++ portfolio.js
@@ -3,23 +3,28 @@ let navbar=document.querySelector('.navbar');
 let sections=document.querySelectorAll('section');
 let navLinks=document.querySelectorAll('header nav a');
 
-window.onscroll=()=>{
-  sections.forEach(sec=>{
-    let top=window.scrollY;
-    let offset=sec.offsetTop-150;
-    let height=sec.offsetHeight;
-    let id=sec.getAttribute('id');
 
-    if(top=>offset && top < offset + height){
-      navLinks.forEach(links=>{
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
         links.classList.remove('active');
-        document.querySelector('header nav a [href*=' + id + ']').classList.add('active')
-      })
+      });
+      let activeLink = document.querySelector('header nav a[href*="' + id + '"]');
+      if (activeLink) {
+        activeLink.classList.add('active');
+      }
     }
-  })
-}
+  });
+};
 
-menuIcon.onclick =()=>{
+
+menuIcon.onclick = () =>{
   menuIcon.classList.toggle('bx-x');
   navbar.classList.toggle('active');
 }
